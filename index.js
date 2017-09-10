c9346d399eaa6cf2a6f51352017178cac901fc84
Burt Joestar <kyleburton515@gmail.com>
2017-09-10 13:13:36 -0400

    SM-2: Added winston for logging to file
    Logs will now output to salt-miner.log in the directory of index.js

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ require('dotenv').config();
 const io = require('socket.io-client');
 const restify = require('restify-clients');
 const MongoClient = require('mongodb').MongoClient;
+const winston = require('winston');
 
 const saltyIo = 'ws://www-cdn-twitch.saltybet.com:1337/';
 const saltyApi = 'http://www.saltybet.com/';
 const statePath = '/state.json';
 const saltStateMineShaftUri = "mongodb://salty-visor:" + process.env.DB_PASS + "@salty-state-shaft-0-shard-00-00-yngvz.mongodb.net:27017,salty-state-shaft-0-shard-00-01-yngvz.mongodb.net:27017,salty-state-shaft-0-shard-00-02-yngvz.mongodb.net:27017/<DATABASE>?ssl=true&replicaSet=salty-state-shaft-0-shard-0&authSource=admin";
 
+const logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Console),
+        new (winston.transports.File)({filename: 'salt-miner.log'})
+
+    ]
+});
+
+function closeDb(db) {
+    db.close().then(function() {
+        logger.info('MongoDB connection closed');
+    }).catch(function(error) {
+        logger.info('Error closing MongoDB connection', error);
+    });
+}
 
 //Connect to the mine shaft
 MongoClient.connect(saltStateMineShaftUri, null, function(err, db) {
@@ -19,38 +35,41 @@ MongoClient.connect(saltStateMineShaftUri, null, function(err, db) {
     });
     console.log('connected');
 
+    logger.info('Connected to MongoDB Instance');
+
     socket.on('connect_error', function(error) {
-        console.log('Socket - error connecting ' + error.message);
-        db.close();
+        logger.info('Unable to open socket to: ' + saltyIo, error);
+        closeDb(db);
     });
 
     //Connect to the salty socket
     socket.on('connect', function() {
         var isPersisting = false;
 
+        logger.info('Socket connected to: ' + saltyIo);
+
         const collection = db.collection('state');
 
-        console.log('Socket - io connected');
-
         socket.on('message', function() {
-            console.log('Socket - received message');
-            client.get(statePath, function(err, req, res, obj) {
-                console.log('Rest - Get received');
-                if (!isPersisting && obj && obj.status && (obj.status === "1" || obj.status === "2")) {
+            logger.info('Received message, requesting state from ' + saltyApi + statePath);
+            client.get(statePath, function(err, req, res, state) {
+                logger.info('Received state');
+                if (!isPersisting && state && state.status && (state.status === "1" || state.status === "2")) {
                     isPersisting = true;
-                    console.log('DB - persisting...');
-                    collection.insertOne(obj).then(function() {
-                        console.log('DB - persisted');
+                    logger.info('DB - state persisting...', state);
+                    collection.insertOne(state).then(function() {
+                        logger.info('DB - state persisted successful');
                         isPersisting = false;
+                    }).catch(function(error) {
+                        logger.info('DB - Error persisting state', error);
                     });
                 }
             });
         });
 
         socket.on('disconnect', function() {
-            console.log('Socket - io disconnected');
-            db.close();
+            logger.info('Socket disconnected');
+            closeDb(db);
         });
     });
-
 });
